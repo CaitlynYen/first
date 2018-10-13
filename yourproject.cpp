8ba4814e5d37aedb127a76b3dabc1aa4fcdf79ae
Caitlyn <catcool19@gmail.com>
2018-10-13 23:05:55 +0800

    second commit

--- yourproject.cpp
+++ yourproject.cpp
@@ -37,24 +37,25 @@ int main(int argc, char *argv[])
 
 		fileout.open(out.c_str());
 		int count = 0;
-		for (int i = 1; i < row - 1; i++)
-			for (int j = 1; j< column - 1; j++)
-				if (mat[i][j] >= mat[i - 1][j])
-					if (mat[i][j] >= mat[i + 1][j])
-						if (mat[i][j] >= mat[i][j - 1])
-							if (mat[i][j] >= mat[i][j + 1])
+		for (int i = 0; i < row; i++)
+			for (int j = 0; j < column; j++)
+				if (j == column -1 || mat[i][j] >= mat[i][j + 1])
+					if (j == 0 || mat[i][j] >= mat[i][j - 1])
+						if (i == row - 1 || mat[i][j] >= mat[i + 1][j])
+							if (i == 0 || mat[i][j] >= mat[i - 1][j])
 							{
 								count++;
-								r.push_back(i + 1);
-								c.push_back(j + 1);
+								r.push_back(i);
+								c.push_back(j);
 							}
 		fileout << count << endl;
+		cout << count << endl;
 		for (int i = 0; i < r.size(); i++) 
 		{
-				fileout << r[i] << " ";
-				cout << r[i] << " ";
-				fileout << c[i] << " ";
-				cout << c[i] << " ";
+				fileout << r[i] + 1 << " ";
+				cout << r[i] + 1 << " ";
+				fileout << c[i] + 1 << " ";
+				cout << c[i] + 1 << " ";
 				fileout << endl;
 				cout << endl;
 		}
